d98af692e458b5191d71ccfc95469eea404019ad
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-09-04 09:44:16 +0100

    fixes made after database was changed

--- build/configurations/index.js
+++ build/configurations/index.js
@@ -5,13 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.database = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 let conn;
 const database = async () => {
     if (conn) {
         return conn;
     }
     else {
-        conn = await mongoose_1.default.connect("mongodb+srv://andaobong:8RhWB1aDgVSAtdr2@cluster0.twfwc1c.mongodb.net/oasis_hrm", {
+        conn = await mongoose_1.default.connect(`${process.env.MONGO_URI}`, {
             serverSelectionTimeoutMS: 10000,
             socketTimeoutMS: 45000
         });
